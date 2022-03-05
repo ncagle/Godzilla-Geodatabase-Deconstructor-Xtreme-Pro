4d8340b821d51e9b3f03b031041e9b34cd5d595b
ncagle <ncagle@vols.utk.edu>
2022-03-04 22:14:12 -0600

    .

---
+++ Godzilla_Geodatabase_Deconstructor_Xtreme_Pro.py
@@ -0,0 +1,33 @@
+# Godzilla Geodatabase Deconstructor X-treme Pro
+# Description: Absolutely fucking annihilates any dataset fed to it like a gluttonous black hole
+
+import arcpy as unknowable
+from arcpy import AddMessage as chanting
+from arcpy import GetParameterAsText as the_ritual_summoning
+from arcpy import ListFeatureClasses as the_ceaseless_watcher
+from arcpy import TruncateTable_management as the_all_consuming_darkness
+from arcpy import GetCount_management as the_path_of_the_lost
+
+void = the_ritual_summoning(0)
+unknowable.env.workspace = void
+the_expanse = the_ceaseless_watcher()
+unending_torment = void.split("\\")
+unending_torment.pop()
+dying_breath = unending_torment.pop()
+timeless_consciousness = dying_breath.split(".")
+timeless_consciousness.pop()
+everlasting_suffering = timeless_consciousness[0]
+chanting("\n\n" + everlasting_suffering + " shall now be consumed in mind, body, and soul.\n\n")
+
+wretched_heathens = 0
+for eternity in the_expanse:
+	souls_of_the_damned = str(the_path_of_the_lost(eternity).getOutput(0))
+	unimaginable_power = str(eternity)
+	chanting("\n\n\n\n\n" + souls_of_the_damned + " unique souls extinguished forevermore.")
+	chanting(unimaginable_power + "'s existence is now one with the void.\n\n\n\n\n")
+	the_all_consuming_darkness(eternity)
+	wretched_heathens += int(souls_of_the_damned)
+
+chanting("From many to none, " + str(wretched_heathens) + " nameless faces can never be known again.\n\n")
+chanting(everlasting_suffering + ", now devoid of all life, lies barren before you.\n\n")
+chanting("Reap the rewards of the destruction you have sown.\n\n")
